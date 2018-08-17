187780fbb8483e69a5548daa0c82fc49af1c63c3
Rafał <r.marzewski@gmail.com>
2018-08-17 19:36:46 +0200

    mirrorBits.cs

---
+++ mirrorBits.cs
@@ -0,0 +1,36 @@
+﻿/*
+Reverse the order of the bits in a given integer.
+
+Example
+
+For a = 97, the output should be
+mirrorBits(a) = 67.
+
+97 equals to 1100001 in binary, which is 1000011 after mirroring, and that is 67 in base 10.
+
+For a = 8, the output should be
+mirrorBits(a) = 1.
+*/
+
+using System;
+
+namespace mirrorBits
+{
+    class Program
+    {
+        static int mirrorBits(int a)
+        {
+            string str = Convert.ToString(a, 2);
+            char[] arr = str.ToCharArray();
+            Array.Reverse(arr);
+            str = new string(arr);
+            return Convert.ToInt32(str, 2);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(mirrorBits(97));
+            Console.ReadKey();
+        }
+    }
+}
