ff05dbbbc69bcc921aa4879ef562f53b9758f3ef
Rafał <r.marzewski@gmail.com>
2018-08-17 19:25:25 +0200

    add */

--- rangeBitCount.cs
+++ rangeBitCount.cs
@@ -1,3 +1,4 @@
+/*
 rangeBitCount
 You are given two numbers a and b where 0 ? a ? b. 
 Imagine you construct an array of all the integers from a to b inclusive. 
@@ -11,6 +12,7 @@ rangeBitCount(a, b) = 11.
 Given a = 2 and b = 7 the array is: [2, 3, 4, 5, 6, 7]. 
 Converting the numbers to binary, we get [10, 11, 100, 101, 110, 111], 
 which contains 1 + 2 + 1 + 2 + 2 + 3 = 11 1s.
+*/
 
 using System;
 using System.Collections.Generic;
