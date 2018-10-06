e5a9274b10665c32522bad8bcfbcf3d920d31338
Rafał <r.marzewski@gmail.com>
2018-10-06 20:29:37 +0200

    amendTheSentence

---
+++ amendTheSentence/amendTheSentence/Program.cs
@@ -0,0 +1,43 @@
+﻿/*
+Do ciąg s:
+ - Umieść pojedynczą spację między słowami zaczynającymi się wielką literą.
+ - Konwertuj wielkie litery na małe litery.
+
+Przykład:
+Dla s = "CodesignalIsAwesome", oczekiwany wynik:
+amendTheSentence(s) = "codesignal is awesome";
+
+Dla s = "Hello", oczekiwany wynik:
+amendTheSentence(s) = "hello".
+ */
+
+using System;
+
+namespace amendTheSentence
+{
+    class Program
+    {
+        static string amendTheSentence(string s)
+        {
+            string res = Convert.ToString(s[0]);
+            char[] arr = s.ToCharArray();
+
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i] >= 'A' && arr[i] <= 'Z')
+                    res = res + " " + arr[i];
+                else
+                    res = res + arr[i];
+            }
+
+            return res.ToLower();
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(amendTheSentence("codesignal is awesome"));
+            Console.WriteLine(amendTheSentence("Hello"));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ amendTheSentence/amendTheSentence/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+// zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+// powiązane z zestawem.
+[assembly: AssemblyTitle("amendTheSentence")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("amendTheSentence")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
+// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
+// COM, ustaw wartość true dla atrybutu ComVisible tego typu.
+[assembly: ComVisible(false)]
+
+// Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
+[assembly: Guid("22ff0919-13d8-4dfa-ae80-aad74af69fc4")]
+
+// Informacje o wersji zestawu zawierają następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza
+//      Numer kompilacji
+//      Rewizja
+//
+// Możesz określić wszystkie wartości lub użyć domyślnych numerów kompilacji i poprawki
+// przy użyciu symbolu „*”, tak jak pokazano poniżej:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
